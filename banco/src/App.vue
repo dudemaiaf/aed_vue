e50af6fa0a5e4847dca52bd202c872b107f1652b
Eduardo Maia Freire <dudu_maia13@hotmail.com>
2019-04-25 14:16:17 -0400

    Feat: App do banco
    Pequena aplicação simulando uma fila e guiche de banco

---
+++ banco/src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="app">
+    <div id="top" class="top">
+      <img alt="Vue logo" src="./assets/logo.png">
+    </div>
+
+    <div id="middle">
+      <input v-model="message" placeholder="Nome">
+      <input v-model="priority" placeholder="Prioridade">
+      <button id="button">Button</button>
+    </div>
+
+    <div id="bottom">
+      <div id="guiche">
+        <p>Guiche 1</p>
+        <img id="logo" alt="Vue logo" src="./assets/logo.png">
+      </div>
+
+      <div id="guiche">
+        <p>Guiche 2</p>
+        <img id="logo" alt="Vue logo" src="./assets/logo.png">
+      </div>
+
+      <div id="guiche">
+        <p>Guiche 3</p>
+        <img id="logo" alt="Vue logo" src="./assets/logo.png">
+      </div>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+/* eslint-disable */
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#logo {
+  padding-top: 5px;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 25px;
+}
+
+#top.top {
+  border-radius: 25px;
+  background-color: #ccc;
+}
+
+#bottom {
+  background-color: lightgreen;
+  column-count: 3;
+}
+
+#p {
+  padding: 0px;
+}
+
+#button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+</style>
